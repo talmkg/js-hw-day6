af4aa505e6ae10c1bfaf6e662fefd362b6a035e2
Tim Afanasiev <soupsoupchannel@gmail.com>
2022-10-04 14:28:28 +0200

    yeah

---
+++ hw.js
@@ -0,0 +1,8 @@
+let myFirstName = "Tim";
+let myLastName = "Afanasiev";
+let space = " ";
+console.log("Hey! My name is " + myFirstName + space + myLastName);
+let myAge = 18;
+let currentYear = 2022;
+let birthYear = currentYear - myAge;
+console.log("I was born in: " + birthYear);
